9765d608e971e4f5845bffeba7133a737f9cb218
Frank Denis <github@pureftpd.org>
2015-04-01 22:57:12 +0200

    IpAddr is back

--- src/geoip/lib.rs
+++ src/geoip/lib.rs
@@ -5,7 +5,7 @@
 #![warn(non_camel_case_types,
         non_upper_case_globals,
         unused_qualifications)]
-#![feature(libc)]
+#![feature(libc, ip_addr)]
 
 extern crate libc;
 extern crate rustc_serialize;
@@ -14,13 +14,8 @@ extern crate geoip_sys;
 use libc::{c_char, c_int, c_ulong};
 use std::ffi;
 use std::fmt;
-use std::net::{Ipv4Addr, Ipv6Addr};
+use std::net::IpAddr;
 use std::path::Path;
-
-pub enum IpAddr {
-    V4(Ipv4Addr),
-    V6(Ipv6Addr)
-}
 
 enum Charset {
     UTF8 = 1
